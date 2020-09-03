e58e9d534d8ca7422413ddaa02bb2ec0d8530ee4
Haflidadottir, Birna <birna.haflidadottir@sabre.com>
2020-09-03 17:52:19 +0000

    Algorithm written for sequence

--- v5/sequence.py
+++ v5/sequence.py
@@ -1,1 +1,7 @@
 n = int(input("Enter the length of the sequence: ")) # Do not change this line
+
+first_num is 1
+sec_num is 2
+third_num is 3
+next_num is the first 3 numbers combined
+then same next loop until we have reached n numbers
